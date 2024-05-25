0a79a842d7320dac5a47c847b94dbf0dc69f06c8
khadija-NEVERLAND <elbachourikadija@gmail.com>
2024-05-25 13:22:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
-// Utilisation de l'élément audio pour la synthèse vocale
-function speak(text) {
-    const utterance = new SpeechSynthesisUtterance(text);
-    const audio = new Audio();
-    audio.src = `https://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=tw-ob&q=${encodeURIComponent(text)}`;
-    audio.play();
-}
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
 
-// Utilisation de l'élément textarea pour la reconnaissance vocale
-function listen() {
-    const recognition = new window.webkitSpeechRecognition();
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => {
+        voiceSelect.options[i] = new Option(voice.name, i);
+    });
+};
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
+
+document.getElementById("click_to_convert").addEventListener('click', function () {
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
     recognition.interimResults = true;
-    recognition.continuous = false;
 
-    recognition.onresult = function(event) {
-        const transcript = Array.from(event.results)
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
             .map(result => result[0])
             .map(result => result.transcript)
             .join('');
 
-        document.getElementById("convert_to_text").innerText = transcript;
-    };
-
-    recognition.onerror = function(event) {
-        console.error('Speech recognition error', event);
-        alert('Error occurred in speech recognition: ' + event.error);
-    };
+        document.getElementById("convert_to_text").innerHTML = transcript;
+    });
 
     recognition.start();
-}
-
-// Ajoutez des gestionnaires d'événements pour appeler les fonctions ci-dessus
-
-document.querySelector("button").addEventListener("click", () => {
-    speak(document.querySelector("textarea").value);
-});
-
-document.getElementById("click_to_convert").addEventListener('click', () => {
-    listen();
 });
